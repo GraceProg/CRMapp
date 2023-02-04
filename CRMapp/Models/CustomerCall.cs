fac5d9ece00b3c6b86e7235c16b999a5526ffc20
Tiwa Grace <gtiwa712@gmail.com>
2023-02-04 20:48:54 +0100

    Add project files.

---
+++ CRMapp/Models/CustomerCall.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRMapp.Models
+{
+    public class CustomerCall
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateOnly DateOfCall { get; set; }
+
+        public TimeOnly TimeOfCall { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Description { get; set; }
+    }
+}
