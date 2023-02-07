ed95e7c2b02f228895ee1309957bc92cb73aad13
GraceProg <124438859+GraceProg@users.noreply.github.com>
2023-02-07 21:17:59 +0100

    css file, js file and the layout for the Navbar

---
+++ js/site.js
@@ -0,0 +1,11 @@
+﻿
+$(document).ready(function () {
+    $('#table').DataTable({
+        "ordering": true,
+        "columnDefs": [{
+            "targets": [1,2,3,4],
+            "ordorable": false
+        }]
+    });
+});
+
